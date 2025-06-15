2e0c823b59db0e916a64ce010e30d76bfb49df1b
FATIMAH BADMUS <95576634+Codedivah@users.noreply.github.com>
2025-06-15 22:18:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-    const taskList = document.getElementById('task-list');
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
     let tasks = [];
 
     
